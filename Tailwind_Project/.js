26e7e3fb8b224042da207a53ef0e0bf7ae6fcbfa
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-05-13 02:19:33 +0530

    Tailwind Project

---
+++ Tailwind_Project/.js
@@ -0,0 +1,4 @@
+//50 to 900 
+// Breakpoints ;- sm:grid-cols-2 meaning - when the screen size is small then it will be 2 columns
+// md:grid-cols-3 meaning - when the screen size is medium then it will be 3 columns
+// lg:grid-cols-4 meaning - when the screen size is large then it will be 4 columns
